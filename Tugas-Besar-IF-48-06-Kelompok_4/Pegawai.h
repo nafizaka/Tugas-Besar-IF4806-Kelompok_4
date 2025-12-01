a9e6fff26e2226d13c7e36a53ee9801f04311256
Pangeran Decaesario <pangeranclev@gmail.com>
2025-12-01 23:54:51 +0700

    menambahkan feature adt child(pegawai)

--- Tugas-Besar-IF-48-06-Kelompok_4/Pegawai.h
+++ Tugas-Besar-IF-48-06-Kelompok_4/Pegawai.h
@@ -1,7 +1,32 @@
 #ifndef PEGAWAI_H_INCLUDED
 #define PEGAWAI_H_INCLUDED
+#include <iostream>
+using namespace std;
+typedef struct elemenpegawai *adrPegawai;
 
-typedef int b;
-
-
+struct Pegawai {
+    string nama;
+    string id;
+    string jabatan;
+    int gaji;
+    bool status;
+};
+struct elemenPegawai {
+    Pegawai info;
+    adrPegawai next;
+};
+struct ListPegawai {
+    adrPegawai first;
+};
+void createListPegawai(ListPegawai &L);
+bool isEmptyPegawai(ListPegawai L);
+adrPegawai createElemenPegawai(Pegawai x);
+void insertFirstPegawai(ListPegawai &L, adrPegawai P);
+void insertLastPegawai(ListPegawai &L, adrPegawai P);
+void insertAfterPegawai(ListPegawai &L, adrPegawai Prec, adrPegawai P);
+void deleteFirstPegawai(ListPegawai &L, adrPegawai &P);
+void deleteLastPegawai(ListPegawai &L, adrPegawai &P);
+void deleteAfterPegawai(ListPegawai &L, adrPegawai Prec, adrPegawai &P);
+adrPegawai searchPegawai(ListPegawai L, string nama);
+void printPegawai(ListPegawai L);
 #endif // PEGAWAI_H_INCLUDED
